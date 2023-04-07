297119da54cdf3e84b173b9d37446d0daa8fe258
ErckLima <erickfalubay@gmail.com>
2023-04-06 21:26:24 -0300

    Responsivo

---
+++ js.js
@@ -0,0 +1,11 @@
+const img1 = document.getElementById("car1")
+const img2 = document.getElementById("car2")
+const img3 = document.getElementById("car3")
+
+if (Window.matchMedia("(max-width: 640px)".match)){
+    img1.src = "./imgs_mobile/Nike100px.jpg"
+    img2.src = "./imgs_mobile/Nike100px.jpg"
+    img3.src = "./imgs_mobile/Nike100px.jpg"
+
+}
+
